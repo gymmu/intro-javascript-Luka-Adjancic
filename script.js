0dc821937658378fa91384804f1d403b9005e8d6
Luka Adjancic <e236879@edu.sbl.ch>
2023-10-17 11:25:22 +0200

    Bis Aufgabe 4 fertig

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+function deleteText() {
+  const deleteTextElem = document.querySelector("#to-delete")
+  deleteTextElem.textContent = "Inhalt gelöscht"
+}
+
+function incrementCounter() {
+  const zahl = document.querySelector("#counter")
+  let num = parseInt(zahl.textContent)
+  num = num + 1
+  zahl.textContent = num
+}
